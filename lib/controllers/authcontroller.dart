17032dfccb343107fe5130b8afb2c5527c928600
anubhav11803451 <anubhav.11803451@lpu.in>
2020-08-28 12:00:11 +0530

    added cloud firestore

--- lib/controllers/authcontroller.dart
+++ lib/controllers/authcontroller.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_app/controllers/usercontroller.dart';
+import 'package:todo_app/models/user.dart';
+import 'package:todo_app/services/database.dart';
 
 class AuthController extends GetxController {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
   Rx<FirebaseUser> _firebaseUser = Rx<FirebaseUser>();
 
-  String get user => _firebaseUser.value?.email;
+  FirebaseUser get user => _firebaseUser.value;
 
   @override
   void onInit() {
@@ -16,11 +19,20 @@ class AuthController extends GetxController {
 
   void createUser(String email, String password, String name) async {
     try {
-      await _auth.createUserWithEmailAndPassword(
+      AuthResult _authResult = await _auth.createUserWithEmailAndPassword(
         email: email.trim(),
         password: password,
       );
-      Get.back();
+      // create userc in firestore
+      UserModel _user = UserModel(
+        uid: _authResult.user.uid,
+        name: name,
+        email: _authResult.user.email,
+      );
+      if (await Database().createNewuser(_user)) {
+        Get.find<UserController>().user = _user;
+        Get.back();
+      }
     } catch (e) {
       Get.snackbar(
         "Error while signing Up",
@@ -34,10 +46,12 @@ class AuthController extends GetxController {
 
   void login(String email, String password) async {
     try {
-      await _auth.signInWithEmailAndPassword(
+      AuthResult _authResult = await _auth.signInWithEmailAndPassword(
         email: email.trim(),
         password: password,
       );
+      Get.find<UserController>().user =
+          await Database().getUser(_authResult.user.uid);
     } catch (e) {
       Get.snackbar(
         "Error while signing in",
@@ -52,6 +66,7 @@ class AuthController extends GetxController {
   void signOut() async {
     try {
       await _auth.signOut();
+      Get.find<UserController>().clear();
     } catch (e) {
       Get.snackbar(
         "Error while signing out",
